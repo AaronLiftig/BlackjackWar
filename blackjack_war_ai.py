3d64ee8855e81bfe1944aa8851e9077457f4fa6c
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2020-08-08 11:55:03 -0400

    Create blackjack_war_ai.py

---
+++ blackjack_war_ai.py
@@ -0,0 +1,31 @@
+class BlackjackWarAI:    
+    def CheckHumanNum(self,numOfPlayers,humanPlayerNum):
+        if (not isinstance(humanPlayerNum,int))|(humanPlayerNum > numOfPlayers)|(humanPlayerNum < 0):
+            raise Exception('Enter correct humanPlayerNum')
+
+    def assignAI(self,playerList,numOfPlayers,humanPlayerNum):
+        for playerIndex in range(numOfPlayers):
+            if playerIndex < humanPlayerNum:
+                playerList[playerIndex].human = True
+            else:
+                playerList[playerIndex].human = False
+        return playerList
+
+    def GameAI(self,player,playerList,blackjackList,bustsList,winnerStack,cardValues):
+        tempList = playerList.copy()
+        tempList.remove(player)
+        if all(player.handTotal >= other.handTotal for other in tempList):
+            print('stay\n')
+            player.result = 'stay'
+        else:
+            if player.handTotal <= 16:
+                if player.size >= 1:
+                    print('hit\n')
+                    player.result = 'hit'
+                else:
+                    print('stay\n')
+                    player.result = 'stay'
+            elif player.handTotal > 16:
+                print('stay\n')
+                player.result = 'stay'
+        return player,playerList,blackjackList,bustsList,winnerStack
